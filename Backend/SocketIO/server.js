29ce28933f7bdc043490f367fb15a04fe098cbe8
vickyKumar <csevicky03@gmail.com>
2024-08-25 21:21:48 +0530

    socketurl

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:3002",
+        origin:"https://chatingapp-wsyc.onrender.com/",
         methods:["GET","POST"],
     }
 })
