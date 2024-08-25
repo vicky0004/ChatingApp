682e7de705a524ade67f1b17bc516962786bacea
vickyKumar <csevicky03@gmail.com>
2024-08-25 18:34:40 +0530

    Production code

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
 import cookieParser from "cookie-parser";
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
@@ -26,7 +27,15 @@ try{
 app.use("/api/user",userRoute);
 app.use("/api/message",messageRoute);
 
- 
+// code for deployment
+if(process.env.NODE_ENV === "production"){
+    const dirPath = path.resolve();
+    app.use(express.static("./Frontend/dist"));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(dirPath,"./Frontend/dist","index.html"));
+    })
+}
+
 server.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 })
