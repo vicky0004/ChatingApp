de19c4034f36654436653d73c17fc2b841d054e7
vickyKumar <csevicky03@gmail.com>
2024-08-26 16:31:12 +0530

    changes

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        // origin:"http://localhost:3000", // for local host
+        // origin:"http://localhost:3000", // for local host 
         origin:"https://chatingapp-wsyc.onrender.com/",  // for online
         methods:["GET","POST"],
     }
